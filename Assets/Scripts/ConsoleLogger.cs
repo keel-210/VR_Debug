a8aaeaedd6fd67a6fc01f5b5ce3f00a363bbb676
keel-210 <crafters_touhou@yahoo.co.jp>
2018-04-25 02:17:00 +0900

    Ver1.0
    コンソールログとシーンビューを表示するUIを追加

---
+++ Assets/Scripts/ConsoleLogger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ConsoleLogger : MonoBehaviour
+{
+	[SerializeField] Text text;
+	void Awake ()
+	{
+		Application.logMessageReceived += OnConsoleLog;
+	}
+	void OnConsoleLog (string logText, string stackTrace, LogType type)
+	{
+		if (string.IsNullOrEmpty (logText))
+		{
+			return;
+		}
+		text.text += logText + System.Environment.NewLine;
+	}
+}
---
+++ Assets/Scripts/SceneViewImage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+public class SceneViewImage : MonoBehaviour
+{
+	RenderTexture renderTexture;
+	[SerializeField] RawImage image;
+	void Update ()
+	{
+		Camera cam = SceneView.GetAllSceneCameras () [0];
+		//SceneView.GetAllSceneCameras () [0].targetTexture = renderTexture;
+		Debug.Log (cam.activeTexture);
+		if (cam.activeTexture)
+		{
+			renderTexture = cam.activeTexture;
+			Texture2D tex = new Texture2D (renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+			RenderTexture.active = renderTexture;
+			tex.ReadPixels (new Rect (0, 0, renderTexture.width, renderTexture.height), 0, 0);
+			tex.Apply ();
+			image.texture = tex;
+		}
+	}
+}
